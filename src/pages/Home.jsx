44a0c49fa92b7ffef7d2ad9561652955d86c4ada
ap-lodhi <anilpatel.bcs@gmail.com>
2022-07-29 00:11:34 +0530

    sorting done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Button, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { MediaCard } from "../Componets/Card";
 import { useFetchProducts } from "../hook/fetchProducts";
@@ -6,14 +6,37 @@ import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import LinearProgress from '@mui/material/LinearProgress'
 import axios from "axios";
+
+
+
 export const Home = () => {
     const [page, setPage] = useState(1);
   const [products ,setProducts]=useState([])
     const [load, setLoad] = useState(false);
     const [erro, setErro] = useState(false);
 
-    //const [sort, setSort] = useState("");
+    const [sort, setSort] = useState("");
     const { loading, error, data } = useFetchProducts("http://localhost:8000/products", page)
+
+const handleSort =(value)=>{
+    console.log(value,"vaa");
+    setSort(value);
+    setLoad(true);
+    axios({
+      method: "get",
+      url: `http://localhost:8000/products?_page=${page}&_limit=12&_sort=price&_order=${value}`
+    }).then((res) => {
+      setProducts(res.data);
+      setLoad(false);
+      console.log(res.data)
+    }).catch((err) => {
+      console.log(err);
+      setLoad(false)
+      setErro(true);
+    })
+  }
+  
+
 
 
 
@@ -21,7 +44,7 @@ export const Home = () => {
         setLoad(true);
         axios({
             method: "get",
-            url: `http://localhost:8000/products?_page=${page}&_limit=12`
+            url: `http://localhost:8000/products?_page=${page}&_limit=12` 
         }).then((res) => {
             setProducts(res.data);
             setLoad(false);
@@ -31,14 +54,34 @@ export const Home = () => {
             setLoad(false)
             setErro(true);
         })
+       
     }, [page])
 
     return (
-
+ 
 
         loading ?   <LinearProgress />
             : error ? <h2>Somthing went Wrong</h2>
                 : <React.Fragment> 
+<br />
+<br />
+<FormControl halfWidth>
+              <InputLabel id="demo-simple-select-label">Sort By Price</InputLabel>
+              <br />
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={sort}
+                label="Age"
+                onChange={(e) => {handleSort(e.target.value)}}
+              >
+                <MenuItem value={""}>No Sorting</MenuItem>
+                <MenuItem value={"asc"}>High To Low</MenuItem>
+                <MenuItem value={"desc"}>Low To High</MenuItem>
+              </Select>
+            </FormControl>
+
+
                          <div style={{ display: "grid", gridTemplateColumns: "repeat(4,1fr)", gap: "20px", padding: "20px" }}>
                     {products.map(el => <div key={el.id} className="card_container"><MediaCard  {...el} />
                 </div>)}
